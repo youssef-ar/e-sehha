ac6e12fa6f17d1a66a214902c706bb6aefe45752
Youssef <youssefaridhi8@gmail.com>
2025-04-26 10:36:13 +0200

    feat(users): integrate Swagger for API documentation and update UsersController with Swagger decorators

--- apps/users/src/users.controller.ts
+++ apps/users/src/users.controller.ts
@@ -2,18 +2,25 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from '@app/shared-auth/guards/auth.guard';
 import { CurrentUser } from '@app/shared-auth';
-
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+@ApiTags('Users')
+@UseGuards(AuthGuard)
 @Controller()
+@ApiBearerAuth("access-token")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   @Get('/me')
-  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: 'Get current user' })
+  @ApiResponse({ status: 200, description: 'Current user' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
   async getMe(@CurrentUser('id') id: string){
     return this.usersService.getMe(id);
   }
 
   @Get()
-  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, description: 'All users' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
   //@UseGuards(AdminGuard)
   async getUsers(){
     return this.usersService.getUsers();
