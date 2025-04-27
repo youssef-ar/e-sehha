43b06542fcbb18c33d01ccce3f2e4fe7651e689a
Youssef <youssefaridhi8@gmail.com>
2025-04-27 10:40:50 +0200

    refactor: remove e-sehha and patients applications, update nest-cli configuration

--- apps/patients/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { PatientsModule } from './patients.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(PatientsModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
--- apps/patients/src/patients.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PatientsService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
