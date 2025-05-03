9ae7984b5b0f31e083f5b6c5e8e0edee05d5645d
Ret2Hell <mohamedyassine.taieb@insat.ucar.tn>
2025-05-03 18:14:03 +0100

    feat(api-gateway): add rate limiting with nestjs-rate-limiter

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppointmentsModule } from './appointments/appointments.module';
 import { ConfigModule } from '@nestjs/config';
 import { validationSchema } from './config/env.validation';
+import { RateLimiterModule, RateLimiterGuard } from 'nestjs-rate-limiter';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -10,7 +12,20 @@ import { validationSchema } from './config/env.validation';
       validationSchema,
       envFilePath: '.env',
     }),
+    RateLimiterModule.register({
+      for: 'Express',
+      type: 'Memory',
+      keyPrefix: 'global',
+      points: 10,
+      duration: 60,
+    }),
     AppointmentsModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RateLimiterGuard,
+    },
   ],
 })
 export class ApiGatewayModule {}
