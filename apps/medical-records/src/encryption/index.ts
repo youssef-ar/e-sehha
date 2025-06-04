4221f990a83eb8b7cca613b5dcd910a09b04a5bd
Youssef Sghairi <sghairiyoussef10@gmail.com>
2025-06-04 12:19:24 +0100

    feat: implement encryption services for medical records, including data encryption and decryption

---
+++ apps/medical-records/src/encryption/index.ts
@@ -0,0 +1,6 @@
+export * from './encryption.module';
+export * from './encryption.config';
+export * from './crypto.service';
+export * from './data-encryption.service';
+export * from './medical-record-encryption.service';
+export { SENSITIVE_FIELDS, NON_SENSITIVE_FIELDS } from './medical-record-encryption.service';
