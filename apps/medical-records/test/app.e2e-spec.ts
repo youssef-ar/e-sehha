9cb231a06deed7e2e079f970d3c54beb876485ea
Youssef <youssefaridhi8@gmail.com>
2025-06-07 22:25:08 +0100

    test ci pipeline

--- apps/medical-records/test/app.e2e-spec.ts
+++ apps/medical-records/test/app.e2e-spec.ts
@@ -19,6 +19,6 @@ describe('MedicalRecordsController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Hello World');
   });
 });
