78ccd22935185bfd1733aef5d89ff394393a5cfd
Ret2Hell <tayeb1920@gmail.com>
2025-06-08 15:51:11 +0100

    refactor: update CORS configuration

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -9,6 +9,12 @@ async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
   const configService = app.get(ConfigService);
 
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL', 'http://localhost:4000'),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- apps/symptom-checker/src/symptom-checker.service.ts
+++ apps/symptom-checker/src/symptom-checker.service.ts
@@ -9,7 +9,10 @@ export class SymptomCheckerService {
 
   constructor() {
     this.gemini = new GoogleGenerativeAI(
-      process.env.GOOGLE_API_KEY ?? (() => { throw new Error('GOOGLE_API_KEY is not defined'); })()
+      process.env.GOOGLE_API_KEY ??
+        (() => {
+          throw new Error('GOOGLE_API_KEY is not defined');
+        })(),
     );
   }
 
@@ -17,7 +20,7 @@ export class SymptomCheckerService {
     const model = this.gemini.getGenerativeModel({ model: 'gemini-2.0-flash' });
     const symptom = symptoms.symptoms;
     this.logger.debug(`Received symptoms: ${JSON.stringify(symptoms)}`);
-    
+
     const prompt = `You are a medical assistant. Your job is to map symptoms to the most relevant doctor specialty. Respond concisely with only the name of the specialty, the reason why and the severity of the symptoms. This is the patient's message: "${symptom}". 
 Respond only in this object format: {
   "specialty": string,
@@ -32,7 +35,7 @@ If it's not understandable, just return:
 }`;
 
     const result = await model.generateContent(prompt);
-    const textResponse = result.response.text(); 
+    const textResponse = result.response.text();
     this.logger.debug(`LLM response: ${textResponse}`);
 
     return this.extractJsonFromLLMResponse(textResponse);
@@ -40,21 +43,19 @@ If it's not understandable, just return:
 
   private extractJsonFromLLMResponse(responseText: string): any {
     try {
-      const cleaned = responseText
-        .replace(/```json|```/g, '')
-        .trim();
+      const cleaned = responseText.replace(/```json|```/g, '').trim();
 
       const fixed = cleaned.replace(/\n(?!\s*["}])/g, ' ');
 
       const parsed = JSON.parse(fixed);
       this.logger.debug(`Parsed JSON: ${JSON.stringify(parsed)}`);
-      return parsed;
+      return { data: parsed };
     } catch (err) {
       this.logger.error('Failed to parse JSON from LLM response', err);
       return {
         specialty: 'General Practitioner',
         severity: 'Low',
-        reason: 'Failed to parse LLM output. Recommend general consultation.'
+        reason: 'Failed to parse LLM output. Recommend general consultation.',
       };
     }
   }
